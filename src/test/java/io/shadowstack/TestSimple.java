1a552cf3f6890ded057beacafad4514c2013f389
Jason DeMorrow <jason@jasondemorrow.com>
2020-03-25 17:08:02 -0700

    add simple test, update pom.xml in preparation to deploy

---
+++ src/test/java/io/shadowstack/TestSimple.java
@@ -0,0 +1,70 @@
+package io.shadowstack;
+
+import org.junit.jupiter.api.Test;
+
+import static io.shadowstack.Fluently.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TestSimple {
+    public static class Kitten {}
+    public static class Mouse {}
+    public static class Cat {
+        public Kitten feedAndBreedLikeACat(Mouse food) {
+            System.out.println("Feed on a mouse");
+            return new Kitten();
+        }
+    }
+    public static class Egg {}
+    public static class Worm {}
+    public static class Bird {
+        public Egg feedAndBreedLikeABird(Worm food) {
+            System.out.println("Feed on a worm");
+            return new Egg();
+        }
+    }
+
+    @Test
+    public void TestFeedAndBreed() throws AdapterException, NoSuchMethodException {
+        Cat inDisguise = new Cat();
+        Bird butNotReally =
+                shoehorn(inDisguise)
+                    .into(Bird.class)
+                    .routing(
+                        method("feedAndBreedLikeABird")
+                            .to("feedAndBreedLikeACat")
+                            .consuming(
+                                convert(Worm.class)
+                                    .to(Mouse.class)
+                                    .with(
+                                        new ArgumentConverter<Worm, Mouse>() {
+                                            @Override
+                                            public Mouse convert(Worm from) throws AdapterException {
+                                                return new Mouse();
+                                            }
+
+                                            @Override
+                                            public void convert(Worm from, Mouse to) throws AdapterException {}
+                                        }
+                                    )
+                            )
+                            .producing(
+                                convert(Kitten.class)
+                                    .to(Egg.class)
+                                    .with(
+                                        new ArgumentConverter<Kitten, Egg>() {
+                                            @Override
+                                            public Egg convert(Kitten from) throws AdapterException {
+                                                return new Egg();
+                                            }
+
+                                            @Override
+                                            public void convert(Kitten from, Egg to) throws AdapterException {}
+                                        }
+                                    )
+                            )
+                    )
+                    .build();
+        Egg fromAKittenWat = butNotReally.feedAndBreedLikeABird(new Worm());
+        assertNotNull(fromAKittenWat);
+    }
+}
